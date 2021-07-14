229728605ec2e006913af448495a1a2f052125a7
Joseph R Miles <joe@jrm.dev>
2021-07-14 14:46:18 -0700

    fix: installed commonjs plugin for vite

--- examples/counter/vite.config.js
+++ examples/counter/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig, } from "vite";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default defineConfig({
   "root": "./src",
+  "plugins": [
+    commonjs()
+  ],
 });
