30593fb7f26d21ca1cec9fd41084af9f69c48e7c
Joseph R Miles <joe@jrm.dev>
2023-06-01 10:06:23 -0700

    fix: clone old state to avoid pass by reference

--- src/patching.ts
+++ src/patching.ts
@@ -256,14 +256,19 @@ export const patchState = (oldState: any, newState: any): any =>
  * @param store The Zustand API that manages the store we want to patch.
  * @param newState The new state that the Zustand store should be patched to.
  */
-export const patchStore = <S extends State>(
+export const patchStore = <S extends unknown>(
   store: StoreApi<S>,
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   newState: any
 ): void =>
 {
+  // Clone the oldState instead of using it directly from store.getState().
+  const oldState = {
+    ...(store.getState() as Record<string, unknown>),
+  };
+
   store.setState(
-    patchState(store.getState() || {}, newState),
+    patchState(oldState, newState),
     true // Replace with the patched state.
   );
 };
